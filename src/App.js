66996a2cbd5f3117107115416c331d7ff851e42d
Jolusegun <Joyce.o.olusegun@gmail.com>
2021-09-09 03:39:32 -0500

    added real minor data using axios

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
-        <small>
-          This project was coded by Joyce O. Olusegun and {""}
-          <a
-            href="https://github.com/Jolusegun/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </small>
+        <Weather defaultCity="Nashville" />
+        <div>
+          <small>
+            This project was coded by Joyce O. Olusegun and {""}
+            <a
+              href="https://github.com/Jolusegun/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </small>
+        </div>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter your City or State.."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-secondary btn-md w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Nashville</h1>
-      <ul className="Description">
-        <li>Thursday, September 09, 2021, 00:27:25</li>
-        <li>Mostly Cloudy with Light Rain</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Partly Cloudy"
-            id="icon"
-            className="float-left"
-          />
+export default function Weather(props) {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setweatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setweatherData({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      date: "Thursday, September 09, 2021, 00:27:25",
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      precipitation: response.data.rain,
+      city: response.data.name,
+    });
 
-          <span className="temperature">8</span>
-          <span className="unit">°F</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li> Precipitation: 60%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 10 m/kh</li>
-          </ul>
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter your City or State.."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-secondary btn-md w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>Nashville</h1>
+        <ul className="Description">
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              id="icon"
+              className="float-left"
+            />
+
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">°F</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: {Math.round(weatherData.precipitation)}%</li>
+              <li>Humidity: {Math.round(weatherData.humidity)}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} m/kh</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "13180cac35345d5f25e6b2fd362d032f";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..";
+  }
 }
