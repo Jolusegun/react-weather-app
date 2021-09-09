1bf3ad65ae6c52689fd42526670d0ccd06c703c2
Jolusegun <Joyce.o.olusegun@gmail.com>
2021-09-08 19:57:24 -0500

    Light update to js before real project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <div>
+        <small>
+          This project was coded by Joyce O. Olusegun and {""}
+          <a
+            href="https://github.com/Jolusegun/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </small>
+      </div>
     </div>
   );
 }
