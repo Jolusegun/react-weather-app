91f030fd490a28d057c680d3d487de2f90691ff8
Jolusegun <Joyce.o.olusegun@gmail.com>
2021-09-13 23:43:25 -0500

    Added new component with icon, Open Weather API with imported icons

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: response.data.icon,
+      icon: response.data.weather[0].icon,
       precipitation: response.data.rain,
       city: response.data.name,
     });
